3a74c04b723335cc15b235fde2028c866b2f04e9
Таня Цапенко <tashatsapenko@MBP-Tana.lan>
2026-06-29 10:32:15 -0600

    chore: remove debug console.log from renderDrawingPng

--- lib/renderDrawingPng.ts
+++ lib/renderDrawingPng.ts
@@ -2,8 +2,7 @@ import sharp from 'sharp'
 import { openingSvgString } from './openingSvgString'
 
 export async function renderDrawingPng(op: any, widthPx: number, heightPx: number): Promise<string> {
-  console.log('renderDrawingPng op:', JSON.stringify({ type: op.type, window_subtype: op.window_subtype, shape: op.shape, width_in: op.width_in, height_in: op.height_in }))
-  const svg = openingSvgString(op)
+const svg = openingSvgString(op)
   const buf = await sharp(Buffer.from(svg))
     .resize(widthPx, heightPx, { fit: 'contain', background: { r: 255, g: 255, b: 255, alpha: 0 } })
     .png()
