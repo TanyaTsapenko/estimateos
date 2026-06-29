c4e952404143580411349458e64e767b959e02bd
Таня Цапенко <tashatsapenko@MBP-Tana.lan>
2026-06-29 10:46:17 -0600

    revert: back to openingSvgString - Turbopack blocks react-dom/server in App Router bundle even in API routes

--- lib/renderDrawingPng.ts
+++ lib/renderDrawingPng.ts
@@ -1,8 +1,8 @@
 import sharp from 'sharp'
-import { renderV2DrawingSvg } from './renderV2DrawingSvg'
+import { openingSvgString } from './openingSvgString'
 
 export async function renderDrawingPng(op: any, widthPx: number, heightPx: number): Promise<string> {
-  const svg = renderV2DrawingSvg(op)
+  const svg = openingSvgString(op)
   const buf = await sharp(Buffer.from(svg))
     .resize(widthPx, heightPx, { fit: 'contain', background: { r: 255, g: 255, b: 255, alpha: 0 } })
     .png()
